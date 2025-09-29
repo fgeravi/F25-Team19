c5d1f81ccbf77e4a52501c440a2d4dc50d44b8c0
NathanK18 <nkitche@clemson.edu>
2025-09-28 20:36:45 -0400

    Added starting place for about page

--- about/views.py
+++ about/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def about_view(request):
+    context = {
+        "team_number": 19,
+        "version": 3,  #sprint 
+        "release_date": "TBD",
+        "product_name": "TBD",
+        "product_description": "TBD",
+    }
+    return render(request, "about/about.html", context)
