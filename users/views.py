86b082623698cfe7e150dc615a27815bd6a39a4c
Txue21 <xue7@g.clemson.edu>
2025-09-30 13:27:12 -0400

    continue fixed some typo to make the code more consistent

--- users/views.py
+++ users/views.py
@@ -19,4 +19,4 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'Registrations/register.html', {'form': form})
+    return render(request, 'registrations/register.html', {'form': form})
