48c13caf4681b25598388c5520cadd1f244bbf3b
NathanK18 <nkitche@clemson.edu>
2025-10-06 00:30:45 -0400

    Added product name to about page

--- about/models.py
+++ about/models.py
@@ -5,6 +5,7 @@ class About(models.Model):
     team_num = models.IntegerField()
     version_number = models.IntegerField()
     release_date = models.DateField(null=True, blank=True)
+    product_name = models.TextField(null=True, blank=True)
     product_description = models.TextField(null=True, blank=True)
 
     class Meta:
