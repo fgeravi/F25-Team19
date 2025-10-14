1f6a6015f804a2afc603fcf68db85324dd16a262
thezachwooten <zbwoote@clemson.edu>
2025-10-14 11:10:20 -0400

    Added loginAttempt model
    added loginattempt model to audit/model.py

--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# audit model for login attempts
+class LoginAttempt(models.Model):
+    username = models.CharField(max_length=150)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    user_agent = models.TextField(null=True, blank=True)
+    successful = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username} - {'Success' if self.successful else 'Failed'} at {self.timestamp}"
