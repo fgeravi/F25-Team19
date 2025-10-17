568945441306e678f36fe543d3f94c5b89d09842
thezachwooten <zbwoote@clemson.edu>
2025-10-17 19:12:30 -0400

    updated 01_static.config
    changed command for collectstatic

--- config/settings.py
+++ config/settings.py
@@ -166,3 +166,6 @@ LOGIN_REDIRECT_URL = 'home'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# WhiteNoise storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
