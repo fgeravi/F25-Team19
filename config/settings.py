c49b8b6868f1c2e0569ddafa99f4c0f2abbbfc11
thezachwooten <zbwoote@clemson.edu>
2025-10-17 18:24:13 -0400

    changed static_root

--- config/settings.py
+++ config/settings.py
@@ -165,4 +165,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'home'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
