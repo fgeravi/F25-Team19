0bb187934db07ccf23afa3b10f060d1b8618db8a
thezachwooten <zbwoote@clemson.edu>
2025-10-17 20:33:11 -0400

    updated create static

--- .platform/hooks/prebuild/01_create_static.sh
+++ .platform/hooks/prebuild/01_create_static.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+set -e
+
+# Make sure staticfiles directory exists
 mkdir -p /var/app/staging/staticfiles
 chmod 755 /var/app/staging/staticfiles
+
+# Activate the virtual environment
+source /opt/python/run/venv/bin/activate
+
+# Run collectstatic using the environment variables already set in EB
+python manage.py collectstatic --noinput
+
+echo "Collectstatic finished successfully!"
