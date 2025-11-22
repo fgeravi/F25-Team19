30c52b277d2454cb2a854462b615d3f2a589592b
fgeravi <fgeravi@clemson.edu>
2025-11-22 18:59:08 -0500

    Implemented password reset UI, Mailtrap SMTP integration, fixed notifications + archive

--- config/settings.py
+++ config/settings.py
@@ -167,3 +167,13 @@ PASSWORD_GRACE_ALLOWLIST = [
     "/accounts/password_change/done/",
     "/static/"
 ]
+
+# Mailtrap SMTP for dev/testing
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "sandbox.smtp.mailtrap.io"
+EMAIL_PORT = 2525
+EMAIL_HOST_USER = "d46148c7c6c01a"              # your Mailtrap username
+EMAIL_HOST_PASSWORD = "22d3039369bd98"    # the full password from Mailtrap
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = "no-reply@gooddrivers.local"
