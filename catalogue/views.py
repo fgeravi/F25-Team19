b48571edbf50ee5b48329bec698084b76f8109f1
Zachary Wooten <zbwoote@clemson.edu>
2025-11-24 13:22:58 -0500

    Fixed some issues with cart
    cart now takes sponsor id from catalogue item. Also fixed issues with ui not being readable at all. still need to fix some other things and then work on ordering

--- catalogue/views.py
+++ catalogue/views.py
@@ -240,11 +240,13 @@ def add_to_cart(request, item_id):
         return redirect("home")
     
     catalogue_item = get_object_or_404(CatalogueItem, id=item_id)
+
     
     if not catalogue_item.is_active:
         messages.error(request, "This item is currently unavailable.")
         org_id = catalogue_item.catalogue.organization.id
-        return redirect("catalogue:view_catalogue", org_id=org_id)
+        sponsor_id = catalogue_item.catalogue.sponsor.id
+        return redirect("catalogue:view_catalogue", sponsor_id=sponsor_id)
     
     cart_item, created = CartItem.objects.get_or_create(
         user=request.user,
@@ -259,8 +261,8 @@ def add_to_cart(request, item_id):
     else:
         messages.success(request, f"'{catalogue_item.product_name}' added to cart.")
     
-    org_id = catalogue_item.catalogue.organization.id
-    return redirect("catalogue:view_catalogue", org_id=org_id)
+    sponsor_id = catalogue_item.catalogue.sponsor.id
+    return redirect("catalogue:view_catalogue", sponsor_id=sponsor_id)
 
 
 @login_required
